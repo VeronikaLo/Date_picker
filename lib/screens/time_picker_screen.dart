cc67031deec03d8b75ca7000184ff4688da0f467
Veronika L <verokipri@yahoo.com>
2022-10-12 01:55:13 +0200

    time picker done

--- lib/screens/time_picker_screen.dart
+++ lib/screens/time_picker_screen.dart
@@ -9,11 +9,42 @@ class TimePicker extends StatefulWidget {
 }
 
 class _TimePickerState extends State<TimePicker> {
+
+  late TimeOfDay time = TimeOfDay.now();
+
+  Future <void> _pickTime()async{
+    TimeOfDay? newTime = await showTimePicker(
+      context: context, 
+      initialTime: time, 
+      );
+
+      if(newTime !=null){ // OK option by Dialog
+        setState(() {
+        time = newTime;
+      });
+      } else {  //Cancel option by Dialog
+        return;
+      }
+      
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final hours = time.hour.toString().padLeft(2,'0');
+    final minutes = time.minute.toString().padLeft(2,'0');
+    return  Scaffold(
       body: Center(
-      child: Text('Time Picker'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Time Picker'),
+          const SizedBox(height: 50),
+          Text('$hours : $minutes'),
+          const SizedBox(height: 70,),
+          ElevatedButton(
+            onPressed: _pickTime, 
+            child: const Text('Select Time'))
+        ],
+      ),
     ),
     );
   }
