55df5a01ae9afcecf30274c27973f5014367d401
Veronika L <verokipri@yahoo.com>
2022-10-12 16:50:13 +0200

    removed banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Date/Time/Range',
       theme: ThemeData(
         primarySwatch:  Colors.cyan,
